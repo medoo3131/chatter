bb2d07480da1cb6363e2f098d9e9f199dfe15ac0
medoo3131 <07810641272sara@gmail.com>
2016-06-29 18:48:33 +0300

    Update chatter.lua

--- plugins/chatter.lua
+++ plugins/chatter.lua
@@ -3,24 +3,24 @@ local function chatter(msg,mico)
 help = [[
 ➖➖➖➖➖➖➖➖➖➖➖
 *commands:*`for admin`
-`/ban` ✴️
+`حضر` ✴️
 *حظر عضو من ارسال رساله بالرد على رسالته*
-`/unban` ✴️
+`الغاء الحضر` ✴️
 *فتح الحظر عن عضو عن طريق الرد على رسالته*
-`/users` ✴️
+`المستخدمين` ✴️
 *معرفه عدد الاعضاء المشتركين*
-`/broadcast` ✴️
+`اذاعة` ✴️
 *ارسل هذا الامر وكل رساله كانت بعده سترسل لجميع المشتركين*
-`/unbroadcast` ✴️
+`ايقاف` ✴️
 *لكي تتوقف ارسال الرسائل وتفعيل الاوامر البقيه*
-`/start` ✴️
+`ابدأ` ✴️
 *لأظهار رساله ترحيب للاعضاٱء*
-`/id` ✴️
+`الايدي` ✴️
 *بالرد على رساله موجهه يضهر لك المعلومات*
 ➖➖➖➖➖➖➖➖➖➖➖
 ]]
 start = [[]]
-You = 171114902
+You = 86309574
 if mico[1] == "start" and not is_add(msg) then
 local keyboard = {}
 
@@ -41,7 +41,7 @@ user = msg.from.username
 end
 local text = "مرحبا بك يا ["..msg.from.first_name.."](www.telegram.me/"..user..")"
 sendMessage(msg.chat.id,text.."\n"..start,true,false,true,key)
-elseif msg.text == "/start" and is_add(msg) then
+elseif msg.text == "ابدأ" and is_add(msg) then
 local keyboard = {}
 
     keyboard.inline_keyboard = {
@@ -60,7 +60,7 @@ user = msg.from.username
 end
 local text = "مرحبا بك يا ["..msg.from.first_name.."](www.telegram.me/"..user..")"
 sendMessage(msg.chat.id,text.."\n"..start,true,false,true,key)
-elseif is_admin(msg) and msg.text == "/users" then
+elseif is_admin(msg) and msg.text == "المستخدمين" then
  	local r = tostring(#add.id)
 
  	local t = string.gsub(r," ","")
@@ -75,14 +75,14 @@ if is_ban(msg) then return end
 if msg.date < os.time() - 5 then -- Ignore old msgs
 		return
 end
-if msg.text == "/unbroadcast" then
+if msg.text == "ايقاف" then
   add.broadcast = "broadcast"
   save_data('mico.db', add)
-  sendMessage(msg.chat.id,"*I will not broadcast any more*",true,false,true)
+  sendMessage(msg.chat.id,"*تــم ايقــاف الاذاعــة 📧*",true,false,true)
 end
-if msg.text ~= "/broadcast" and  add.broadcast ~= "broadcast" then
+if msg.text ~= "اذاعة" and  add.broadcast ~= "broadcast" then
 if is_admin(msg) then
-if msg.text and msg.text ~= "/unbroadcast" then
+if msg.text and msg.text ~= "ايقاف" then
 for k,v in pairs(add.id) do
 sendMessage(v,msg.text,true,false,true)
 end
@@ -94,38 +94,38 @@ end
 end
 end
 elseif msg.text ~= "/" then
-if is_admin(msg) and msg.reply_to_message and msg.reply_to_message.forward_from ~= nil and msg.text == "/ban" then
+if is_admin(msg) and msg.reply_to_message and msg.reply_to_message.forward_from ~= nil and msg.text == "حضر" then
 table.insert(ban.id,msg.reply_to_message.forward_from.id)
-sendMessage(msg.reply_to_message.forward_from.id,"*YOU HAVE BEEN BANNED*",true,false,true)
-elseif is_admin(msg) and msg.reply_to_message and msg.reply_to_message.forward_from ~= nil and msg.text == "/unban" then
+sendMessage(msg.reply_to_message.forward_from.id,"*تــم حضــرك من اســخــدام البــوت 👞❌*",true,false,true)
+elseif is_admin(msg) and msg.reply_to_message and msg.reply_to_message.forward_from ~= nil and msg.text == "الغاء الحضر" then
 for k, v in pairs(ban["id"]) do
 if ( v == msg.reply_to_message.forward_from.id ) then
 table.remove(ban["id"], k)
 end
 end
-sendMessage(msg.reply_to_message.forward_from.id,"*YOU HAVE BEEN unBANNED*",true,false,true)
-elseif is_admin(msg) and msg.reply_to_message and msg.text == "/id" then
+sendMessage(msg.reply_to_message.forward_from.id,"*تــم الســماح لك باســخــدام البــوت ✅🌀*",true,false,true)
+elseif is_admin(msg) and msg.reply_to_message and msg.text == "الايدي" then
  if msg.reply_to_message.forward_from.last_name ~= nil then
  	 last_name = msg.reply_to_message.forward_from.last_name
  else
  	 last_name = ""
  end
  if msg.reply_to_message.forward_from.username ~= nil then
- 	 usernme = "\nUSERNAME : @"..msg.reply_to_message.forward_from.username
+ 	 usernme = "\nالمعرف : @"..msg.reply_to_message.forward_from.username
  else
  	 usernme = ""
  end
- local E = "NAME : "..msg.reply_to_message.forward_from.first_name.." "..last_name..usernme.."\nID :"..msg.reply_to_message.forward_from.id
+ local E = "الاسم : "..msg.reply_to_message.forward_from.first_name.." "..last_name..usernme.."\nالايدي :"..msg.reply_to_message.forward_from.id
  sendMessage(msg.chat.id,E)
 
-elseif is_admin(msg) and msg.text == "/broadcast" then
+elseif is_admin(msg) and msg.text == "اذاعة" then
 
   add.broadcast = "unbroadcast"
-    sendMessage(msg.chat.id,"*I will send every thing u want*",true,false,true)
+    sendMessage(msg.chat.id,"*الآن قــم بكتــابة ما تــريد ارســاله للمســخــدمــين 📩*",true,false,true)
 save_data('mico.db', add)
 
 
-elseif not is_ban(msg) and msg.text ~= "/start" and msg.text ~= "/id" and msg.text ~= "/unban" and msg.text ~= "/ban" and mico[1] == "#@reply@#" then
+elseif not is_ban(msg) and msg.text ~= "ابدأ" and msg.text ~= "الايدي" and msg.text ~= "الغاء الحضر" and msg.text ~= "حضر" and mico[1] == "#@reply@#" then
 
 if is_admin(msg) and msg.reply_to_message and mico[1] == "#@reply@#" then
 forwardMessage(msg.reply_to_message.forward_from.id,msg.from.id,msg.message_id)
@@ -141,15 +141,15 @@ return {
 patterns = {
 
 
-"/ban",
-"/unban",
+"حضر",
+"الغاء الحضر",
 "(in:help)",
-"/users",
-"/broadcast",
-"/unbroadcast",
-"/(start)",
+"المستخدمين",
+"اذاعة",
+"ايقاف",
+"(ابدأ)",
 "/(about)",
-"/id",
+"الايدي",
 "(#@reply@#)",
 },
 
